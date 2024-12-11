b03c2f1e47d7843fbd1996ce945c83bea561e74f
abdelhamid <abdelhamid.hachimi.alaoui@gmail.com>
2024-12-11 08:42:18 +0100

    distant connexion fixer au niveau du serveur

--- rmi_serveur/src/rmi_serveur/Main.java
+++ rmi_serveur/src/rmi_serveur/Main.java
@@ -7,22 +7,22 @@ import java.rmi.server.UnicastRemoteObject;
 
 import rmi_api.ServicePersonne;
 
-
 public class Main {
 
-	
-	
-	public static void main(String[] args) throws RemoteException {
-		
-		System.out.print("Serveur Running "+"\n");	
-		
-		 Registry registry = LocateRegistry.createRegistry(5002);
-		 
-		 ServisePersonneImpl serviceImpl = new ServisePersonneImpl();
-		 
-		 ServicePersonne service = (ServicePersonne) UnicastRemoteObject.exportObject(serviceImpl,0);
-		
-		 registry.rebind("ServicePersonne", service);
-         System.out.println("Service 'ServicePersonne' enregistré et prêt."+"\n");
-	}
+    public static void main(String[] args) throws RemoteException {
+        System.out.print("Serveur Running " + "\n");
+
+        // Définir l'adresse IP du serveur
+        System.setProperty("java.rmi.server.hostname", "192.168.11.106"); // Remplacez par l'IP de votre serveur
+
+        Registry registry = LocateRegistry.createRegistry(5002);
+
+        ServisePersonneImpl serviceImpl = new ServisePersonneImpl();
+
+        ServicePersonne service = (ServicePersonne) UnicastRemoteObject.exportObject(serviceImpl, 0);
+
+        registry.rebind("ServicePersonne", service);
+
+        System.out.println("Service 'ServicePersonne' enregistré et prêt. \n");
+    }
 }
